8046da7ad52e22ca3fa44d85e733b4dd18738142
Divyalakshmi <ndivyalakshmi88@gmail.com>
2024-02-25 18:59:27 +0300

    added professional development section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,26 @@
+//Professional Development - Swipers
+const swiper = new Swiper(".certification-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
 //Website dark/light theme
 
 const themeBtn = document.querySelector(".theme-btn");
