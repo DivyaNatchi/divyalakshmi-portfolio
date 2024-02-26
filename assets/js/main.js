8d8a088573aea358306343cdbac3dd6d3de4e68a
Divyalakshmi <ndivyalakshmi88@gmail.com>
2024-02-26 17:28:34 +0300

    remaining skills, contact, footer section and aria attribute added

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,3 +70,39 @@ ScrollReveal().reveal(".media-icons i", {
   interval: 200,
 });
 ScrollReveal().reveal(".home-img", { delay: 500, origin: "bottom" });
+
+//Scroll to top button
+
+const scrollTopBtn = document.querySelector(".ScrollToTop-btn");
+
+window.addEventListener("scroll", () => {
+  scrollTopBtn.classList.toggle("active", window.scrollY > 50);
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+//Navigation menu items active on page scroll
+
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    let sectionHeight = current.offsetHeight;
+    let sectionTop = current.offsetTop - 50;
+    let id = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.remove("active");
+    }
+  });
+});
